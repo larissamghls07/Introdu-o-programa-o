57d0b7fd3615aba1b5684d2bb9635575a2625138
larissamghls07 <larissamghls07@gmail.com>
2026-03-22 20:57:21 -0300

    Iniciando a lista 2

---
+++ lista_2/exe21.py
@@ -0,0 +1,12 @@
+'''
+prg021 - Escreva um programa que peça dois números e imprima o maior deles.
+'''
+
+n1 = int(input('Digite o n1: '))
+n2 = int(input('Digite o n2: ')) 
+
+if n1>n2:
+    print(f'{n1} é maior que o {n2}.')
+
+else:
+    print(f'{n2} é maior que o {n1}.') 
