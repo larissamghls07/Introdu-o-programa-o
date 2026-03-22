ff4e02f9244f274eae80ced36e13a6009709b4fe
larissamghls07 <larissamghls07@gmail.com>
2026-03-22 19:20:12 -0300

    removendo arquivos do repositorio remoto

--- README.md
+++
@@ -1,1 +0,0 @@
-# Introdu-o-programa-o
--- exe9.py
+++
@@ -1,9 +0,0 @@
-'''
-Escreva um programa que calcule o tempo de uma viagem de carro. Pergunte a
-distância a percorrer e a velocidade média esperada para a viagem.
-
-'''
-distancia = float(input('Digite a sua distância total: '))
-velocidade = float(input('Digite a velocidade média: '))
-tempo = distancia / velocidade
-print('O tempo de viagem é:', tempo, 'segundos.')
