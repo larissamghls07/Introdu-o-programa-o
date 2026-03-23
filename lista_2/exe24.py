afaf876603487893734383b85c3c1cc7acbe3769
larissamghls07 <larissamghls07@gmail.com>
2026-03-22 21:04:59 -0300

    Exe24

---
+++ lista_2/exe24.py
@@ -0,0 +1,9 @@
+'''
+prg024 - Escreva um programa que verifique se uma letra digitada é vogal ou consoante.
+'''
+
+letra = input('Digite uma letra: ')
+if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
+  print('A letra é uma vogal!')
+else:
+  print('A letra é uma consoante!')
