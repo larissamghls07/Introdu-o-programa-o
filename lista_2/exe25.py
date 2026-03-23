15f06e433b7258f2b9abfcfe74c5281a387aa39b
larissamghls07 <larissamghls07@gmail.com>
2026-03-22 21:05:24 -0300

    Exe25

---
+++ lista_2/exe25.py
@@ -0,0 +1,11 @@
+'''
+prg025 - Escreva um programa que leia um número é informe se o mesmo é par ou impar.
+
+'''
+
+num = int(input('Digite um número: '))
+
+if (num % 2) == 0:
+  print('O seu número é par!')
+else:
+  print('O seu número é ímpar!')
