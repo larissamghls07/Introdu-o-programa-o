6eecd33034a58e254a91f434831f0d7f124f8c5b
larissamghls07 <larissamghls07@gmail.com>
2026-06-13 22:15:48 -0300

    Exercício 75 atual

--- LISTAS/lista_3/exe75.py
+++ LISTAS/lista_3/exe75.py
@@ -1,20 +1,42 @@
 '''
 prg075 – Faça um programa parecido com os anteriores, mas agora com a validação do CNPJ.
 '''
-soma, soma2 = 0, 0 
+soma, soma2, digito1, digito2 = 0, 0, 0, 0 
 
 cnpj = input(f'Infome o seu CNPJ: ')
 
+#o cnpj precisa ter 12 elementos 
 if int(len(cnpj)) != 12:
     print(f'Informe um CNPJ válido!')
 else:
     print('podemos continuar')
 
+int(cnpj)
 
+#verificação do primeiro dígito 
 for i in range(4):
-    soma += int(cnpj[i]) * (5 - i)
+    soma += cnpj[i] * (5 - i)
 
 for j in range(8):
-    soma += int(cnpj[j+4]) * (9 - j)
-    print(int(cnpj[j+4]) * (9 - j))
+    soma += cnpj[j+4] * (9 - j)
 
+if (soma % 11) < 2:
+    digito1 = 0
+else:
+    digito1 = 11 - (soma % 11) 
+#fim da verificação do primeiro dígito 
+
+#verificação do segundo dígito 
+for l in range(5):
+    soma2 += cnpj[l] * (6 - l)
+
+for k in range(7):
+    soma2 += cnpj[k+5] * (9 - k)
+
+if (soma2 % 11) < 2:
+    digito2 = 0
+else:
+    digito2 = 11 - (soma2 % 11)
+#fim da verificação do segundo dígito
+
+print(digito1, digito2)
